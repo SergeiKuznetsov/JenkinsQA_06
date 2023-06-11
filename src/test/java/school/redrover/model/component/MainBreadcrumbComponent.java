69a3dfc8697dd0618031365deba64549a0ee7afb
Kk-788 <kyrylo788@gmail.com>
2023-06-11 15:42:36 -0700

    Minor refactoring: simplify method names and adjust formatting

--- src/test/java/school/redrover/model/component/MainBreadcrumbComponent.java
+++ src/test/java/school/redrover/model/component/MainBreadcrumbComponent.java
@@ -1,13 +1,11 @@
 package school.redrover.model.component;
 
-import org.openqa.selenium.By;
+import school.redrover.model.*;
+import school.redrover.model.base.*;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import school.redrover.model.*;
-import school.redrover.model.base.BaseComponent;
-import school.redrover.model.base.BaseMainHeaderPage;
-import school.redrover.model.base.BasePage;
 
 public class MainBreadcrumbComponent<Page extends BasePage<?, ?>> extends BaseComponent<Page> {
 
@@ -15,46 +13,62 @@ public class MainBreadcrumbComponent<Page extends BasePage<?, ?>> extends BaseCo
         super(page);
     }
 
-    public String getFullBreadcrumbText() {
+    public String getFullBreadcrumbPath() {
+
         return getWait5()
-                .until(ExpectedConditions.visibilityOfElementLocated
-                        (By.xpath("//div[@id='breadcrumbBar']")))
-                .getText()
-                .replaceAll("\\n", " > ")
-                .trim();
+            .until(ExpectedConditions.visibilityOfElementLocated
+                    (By.xpath("//div[@id='breadcrumbBar']")))
+            .getText()
+            .replaceAll("\\n", " > ")
+            .trim()
+        ;
+
     }
 
     public int countBreadcrumbItems()  {
 
         return this
-                .getFullBreadcrumbText()
-                .replaceAll("[^>]", "")
-                .trim()
-                .length() + 1;
+            .getFullBreadcrumbPath()
+            .replaceAll("[^>]", "")
+            .trim()
+            .length()
+            + 1
+        ;
+
+    }
+
+
+    public WebElement getListItem(String listItemName) {
+
+        return getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//li[@class='jenkins-breadcrumbs__list-item']" +
+                        "/a[text() = '%s']".formatted(listItemName)
+                )
+            )
+        );
+
+    }
+
+    public <ReturnedPage extends BaseMainHeaderPage<?>> ReturnedPage clickListItem(String listItemName, ReturnedPage returnedPage){
+
+        getListItem(listItemName).click();
+
+        return returnedPage;
 
     }
 
     public MainPage navigateToDashboardByBreadcrumb() {
 
-        getListItemOfBreadcrumb("Dashboard").click();
+        getListItem("Dashboard").click();
+
         return new MainPage(getDriver());
 
     }
 
-    public WebElement getListItemOfBreadcrumb(String listItemName) {
-
-        return getWait5().until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//li[@class='jenkins-breadcrumbs__list-item']" +
-                        "/a[contains(text(), '" + listItemName + "')]"
-                )
-            )
-        );
-    }
-
-    public MainBreadcrumbComponent<Page> openDropdownMenuOfListItem(String listItemName) {
+    public MainBreadcrumbComponent<Page> openDropdownOfListItem(String listItemName) {
 
         Actions actions = new Actions(getDriver());
-        final WebElement listItem = this.getListItemOfBreadcrumb(listItemName);
+        final WebElement listItem = this.getListItem(listItemName);
         final WebElement chevron = listItem.findElement(By.xpath("./button"));
 
         actions.moveToElement(listItem).perform();
@@ -65,21 +79,16 @@ public class MainBreadcrumbComponent<Page extends BasePage<?, ?>> extends BaseCo
 
     }
 
-    public <ReturnedPage extends BaseMainHeaderPage<?>> ReturnedPage clickBreadcrumbItem(String listItemName, ReturnedPage pageToReturn){
 
-        getListItemOfBreadcrumb(listItemName).click();
-        return pageToReturn;
+    public <ReturnedPage extends BaseMainHeaderPage<?>> ReturnedPage clickDropdownOption(String optionText, ReturnedPage returnedPage) {
 
-    }
+        getWait5()
+            .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+                    "//div[@id='breadcrumb-menu']//span[text() = '%s']"
+                    .formatted(optionText)))
+            ).click();
 
-    public <ReturnedPage extends BaseMainHeaderPage<?>> ReturnedPage clickDropdownOption(String optionText, ReturnedPage pageToReturn) {
-
-        WebElement dropdownMenu = getDriver().findElement(By.xpath("//div[@id='breadcrumb-menu']"));
-        WebElement option = dropdownMenu.findElement(By.xpath(".//span[contains(text(), '" + optionText + "')]"));
-
-        option.click();
-
-        return pageToReturn;
+        return returnedPage;
 
     }
 
